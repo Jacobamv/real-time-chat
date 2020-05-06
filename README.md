d52c4972d7a0f6248aab2beb66238309cb8e5dcc
Jacob Akhmedov <56736396+Jacobamv@users.noreply.github.com>
2020-05-06 17:05:26 +0500

    Ver2
    Ver2

---
+++ README.md
@@ -0,0 +1,10 @@
+Real-Time-Chat on Python Flask
+=============
+
+Better contact management.
+
+To get started:
+ - `git clone https://github.com/Jacobamv/real-time-chat.git`
+ - `pip install -r requirements.txt`
+ - `python app.py`
+ - `go to http://127.0.0.1:5000`
--- app.py
+++ app.py
@@ -4,23 +4,35 @@ from models import *
 from peewee import *
 import sqlite3
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Jacob ahuenniy proger'
+app.config['SECRET_KEY'] = 'super secret key '
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
 	if session.get("user") is None:
 		return redirect('/login')
-	else:
-		return render_template('index.html')
+
+
+	name = session.get('user')
+	m = Messages.select(Messages.From, Messages.room).distinct().where(Messages.to == name)
+	return render_template('index.html', name=name, rows=m)
+
+
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def Gotoroom():
 	_name = request.form['user']
+	session['userr'] = _name
 	us = Users.get(Users.name==_name)
 	arr = [_name ,session.get('user')]
 	arr.sort()
 	return redirect('/'+arr[0]+'|'+arr[1],code=302)
+
+
+
 
 @app.route('/<roomname>')
 def Showroom(roomname):
@@ -31,12 +43,17 @@ def Showroom(roomname):
 	c2 = conn.cursor()
 	c2.execute('''SELECT * FROM messages WHERE room = ?;''', (roomname,))
 	rows = c2.fetchall()
-	return render_template('chat.html', messages=rows, user=session.get('user'))
-	сonn.close()
+	return render_template('chat.html', messages=rows, user=session.get('user'), wth=session.get('userr'))
+
+
 
 @app.route('/login')
 def showLogin():
 	return render_template('login.html')
+
+
+
+
 
 @app.route('/login/sign-up', methods=['GET', 'POST'])
 def signUp():
@@ -45,6 +62,11 @@ def signUp():
 	us = Users(name=_name, password=_pass)
 	us.save()
 	return redirect('/', code=302	)
+
+
+
+
+
 
 @app.route('/login/sign-in', methods=['GET', 'POST'])
 def signIn():
@@ -58,6 +80,10 @@ def signIn():
 		return "Password was wrooooong"
 
 
+
+
+
+
 @socketio.on('message')
 def handleMessage(msg):
 	r = session.get('roomname')
@@ -68,8 +94,9 @@ def handleMessage(msg):
 		to = r[0]
 	ms = Messages(message = msg, From = session.get('user'), to = to, room = session.get('roomname'))
 	ms.save()
-	msg = str(session.get('user')) + ' : ' + str(msg)
+	#msg = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">'+ msg +'</div><div class="img_cont_msg"><img class="rounded-circle user_img_msg"></div></div>'
 	send(msg, broadcast = True)
+
 
 
 if __name__ == '__main__':
